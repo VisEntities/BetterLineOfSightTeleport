84958870f5e92eb005dafa14980cd10ecb872e23
Developer <dev@example.com>
2026-03-03 11:14:51 +0100

    - Teleporting to water/ocean now lands you at the surface instead of sending you deep underwater - Added playerboat to the default prioritized entities list - Small code cleanup

--- BetterLineOfSightTeleport.cs
+++ BetterLineOfSightTeleport.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (C) 2024 Game4Freak.io
+ * Copyright (C) 2026 Game4Freak.io
  * This mod is provided under the Game4Freak EULA.
  * Full legal terms can be found at https://game4freak.io/eula/
  */
@@ -12,8 +12,8 @@ using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("Better Line Of Sight Teleport", "VisEntities", "1.0.1")]
-    [Description("Improves the teleportation mechanism of the native teleportlos command.")]
+    [Info("Better Line Of Sight Teleport", "VisEntities", "1.1.0")]
+    [Description("A better and smarter version of the teleportlos command.")]
     public class BetterLineOfSightTeleport : RustPlugin
     {
         #region Fields
@@ -22,7 +22,7 @@ namespace Oxide.Plugins
         private static Configuration _config;
         private const int LAYER_GROUND = Layers.Mask.World | Layers.Mask.Terrain | Layers.Mask.Construction;
         private const float DEBUG_DURATION = 10f;
-    
+
         #endregion Fields
 
         #region Configuration
@@ -32,20 +32,20 @@ namespace Oxide.Plugins
             [JsonProperty("Version")]
             public string Version { get; set; }
 
-            [JsonProperty("Teleport Distance Limit")]
-            public float TeleportDistanceLimit { get; set; }
+            [JsonProperty("Maximum Teleport Distance (meters)")]
+            public float MaximumTeleportDistance { get; set; }
 
-            [JsonProperty("Entity Short Prefab Names To Prioritize")]
-            public List<string> EntityShortPrefabNamesToPrioritize { get; set; }
-            
-            [JsonProperty("Radius For Detecting Nearby Entities")]
-            public float RadiusForDetectingNearbyEntities { get; set; }
+            [JsonProperty("Prioritized Entities (short prefab names)")]
+            public List<string> PrioritizedEntities { get; set; }
 
-            [JsonProperty("Number Of Check Points During Teleportation")]
-            public int NumberOfCheckPointsDuringTeleportation { get; set; }
+            [JsonProperty("Entity Detection Radius (meters)")]
+            public float EntityDetectionRadius { get; set; }
 
-            [JsonProperty("Enable Debug")]
-            public bool EnableDebug { get; set; }
+            [JsonProperty("Teleport Checkpoints (higher = more precise)")]
+            public int TeleportCheckpoints { get; set; }
+
+            [JsonProperty("Enable Debug Mode")]
+            public bool EnableDebugMode { get; set; }
         }
 
         protected override void LoadConfig()
@@ -78,6 +78,9 @@ namespace Oxide.Plugins
             if (string.Compare(_config.Version, "1.0.0") < 0)
                 _config = defaultConfig;
 
+            if (string.Compare(_config.Version, "1.1.0") < 0)
+                _config = defaultConfig;
+
             PrintWarning("Config update complete! Updated from version " + _config.Version + " to " + Version.ToString());
             _config.Version = Version.ToString();
         }
@@ -87,8 +90,8 @@ namespace Oxide.Plugins
             return new Configuration
             {
                 Version = Version.ToString(),
-                TeleportDistanceLimit = 900f,
-                EntityShortPrefabNamesToPrioritize = new List<string>
+                MaximumTeleportDistance = 900f,
+                PrioritizedEntities = new List<string>
                 {
                     "bradleyapc",
                     "patrolhelicopter",
@@ -98,11 +101,12 @@ namespace Oxide.Plugins
                     "rhib",
                     "rowboat",
                     "cargoshiptest",
-                    "ch47scientists.entity"
+                    "ch47scientists.entity",
+                    "playerboat"
                 },
-                RadiusForDetectingNearbyEntities = 40f,
-                NumberOfCheckPointsDuringTeleportation = 15,
-                EnableDebug = false
+                EntityDetectionRadius = 40f,
+                TeleportCheckpoints = 15,
+                EnableDebugMode = false
             };
         }
 
@@ -141,30 +145,29 @@ namespace Oxide.Plugins
         private void Teleport(BasePlayer player)
         {
             Ray ray = player.eyes.HeadRay();
-            float maximumDistance = _config.TeleportDistanceLimit;
-            float entityDetectionRadius = _config.RadiusForDetectingNearbyEntities;
-            int numberOfTeleportCheckpoints = _config.NumberOfCheckPointsDuringTeleportation;
-            float distanceBetweenCheckpoints = maximumDistance / numberOfTeleportCheckpoints;
+            float maxDistance = _config.MaximumTeleportDistance;
+            float entityDetectionRadius = _config.EntityDetectionRadius;
+            int checkpoints = _config.TeleportCheckpoints;
+            float checkpointSpacing = maxDistance / checkpoints;
 
-            Vector3 initialTeleportPosition = ray.origin + ray.direction * maximumDistance;
+            Vector3 teleportPosition = ray.origin + ray.direction * maxDistance;
 
-            if (_config.EnableDebug)
-                DrawUtil.Arrow(player, DEBUG_DURATION, Color.black, ray.origin, initialTeleportPosition, 5.0f);
+            if (_config.EnableDebugMode)
+                DrawUtil.Arrow(player, DEBUG_DURATION, Color.black, ray.origin, teleportPosition, 5.0f);
 
             BaseEntity closestEntity = null;
             float closestDistance = float.MaxValue;
 
-            for (int i = 0; i <= numberOfTeleportCheckpoints; i++)
+            for (int i = 0; i <= checkpoints; i++)
             {
-                Vector3 checkPosition = ray.origin + ray.direction * (i * distanceBetweenCheckpoints);
+                Vector3 checkPosition = ray.origin + ray.direction * (i * checkpointSpacing);
 
-                if (_config.EnableDebug)
+                if (_config.EnableDebugMode)
                     DrawUtil.Sphere(player, DEBUG_DURATION, Color.black, checkPosition, entityDetectionRadius);
 
                 if (i != 0)
                 {
-                    // It has to be this layer; otherwise some entities like the patrol helicopter won't be detected for whatever reason.
-                    List<BaseEntity> entities = FindEntitiesOfType<BaseEntity>(checkPosition, entityDetectionRadius, 1218652417, _config.EntityShortPrefabNamesToPrioritize);
+                    List<BaseEntity> entities = FindEntitiesOfType<BaseEntity>(checkPosition, entityDetectionRadius, 1218652417, _config.PrioritizedEntities);
                     foreach (BaseEntity entity in entities)
                     {
                         float distance = Vector3.Distance(checkPosition, entity.transform.position);
@@ -179,30 +182,39 @@ namespace Oxide.Plugins
 
                     if (closestEntity != null)
                     {
-                        initialTeleportPosition = closestEntity.transform.position;
+                        teleportPosition = closestEntity.transform.position;
 
-                        if (_config.EnableDebug)
+                        if (_config.EnableDebugMode)
                         {
-                            DrawUtil.Box(player, DEBUG_DURATION, Color.green, initialTeleportPosition, 1f);
-                            DrawUtil.Text(player, DEBUG_DURATION, Color.white, initialTeleportPosition, closestEntity.ShortPrefabName);
+                            DrawUtil.Box(player, DEBUG_DURATION, Color.green, teleportPosition, 1f);
+                            DrawUtil.Text(player, DEBUG_DURATION, Color.white, teleportPosition, closestEntity.ShortPrefabName);
                         }
 
                         break;
                     }
                 }
 
-                if (Physics.Raycast(checkPosition, ray.direction, out RaycastHit raycastHit, distanceBetweenCheckpoints, LAYER_GROUND))
+                if (Physics.Raycast(checkPosition, ray.direction, out RaycastHit raycastHit, checkpointSpacing, LAYER_GROUND))
                 {
-                    initialTeleportPosition = raycastHit.point;
+                    teleportPosition = raycastHit.point;
 
-                    if (_config.EnableDebug)
-                        DrawUtil.Box(player, DEBUG_DURATION, Color.green, initialTeleportPosition, 1f);
+                    if (_config.EnableDebugMode)
+                        DrawUtil.Box(player, DEBUG_DURATION, Color.green, teleportPosition, 1f);
 
                     break;
                 }
             }
 
-            player.Teleport(initialTeleportPosition);
+            WaterLevel.WaterInfo waterInfo = WaterLevel.GetWaterInfo(teleportPosition, true, true);
+            if (waterInfo.isValid && teleportPosition.y < waterInfo.surfaceLevel)
+            {
+                teleportPosition.y = waterInfo.surfaceLevel;
+
+                if (_config.EnableDebugMode)
+                    DrawUtil.Box(player, DEBUG_DURATION, Color.cyan, teleportPosition, 1f);
+            }
+
+            player.Teleport(teleportPosition);
         }
 
         #endregion Custom Teleportation
@@ -245,11 +257,6 @@ namespace Oxide.Plugins
             public static void Sphere(BasePlayer player, float durationSeconds, Color color, Vector3 position, float radius)
             {
                 player.SendConsoleCommand("ddraw.sphere", durationSeconds, color, position, radius);
-            }
-
-            public static void Line(BasePlayer player, float durationSeconds, Color color, Vector3 fromPosition, Vector3 toPosition)
-            {
-                player.SendConsoleCommand("ddraw.line", durationSeconds, color, fromPosition, toPosition);
             }
 
             public static void Arrow(BasePlayer player, float durationSeconds, Color color, Vector3 fromPosition, Vector3 toPosition, float headSize)
